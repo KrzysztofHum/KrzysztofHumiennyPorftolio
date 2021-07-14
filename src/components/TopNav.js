f2afab431e1fd990e9a5019f12c86b4fb5ef8bb8
KrzysztofHum <k.humienny1996@protonmail.com>
2021-07-14 12:20:24 +0200

    TopNavMobil

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, { useState } from "react"
+import { FaDownload } from "react-icons/fa"
 
 export default function TopNav() {
-	return <div className="top-nav">
-		<div className="logo-container"></div>
-		<div className="menu-list__icon-container">
-			<a className="menu-icon">
-				<i></i>
-			</a>
-		</div>
-		<ul className="menu-list">
-	<li className="cv-li">
-		<a></a>
-	</li>
-	<li className="contact-li">
-		<a></a>
-	</li>
-		</ul>
-	</div>
+  const [show, setShow] = useState(false)
+  return (
+    <div className="top-nav">
+      <div className="logo-container"></div>
+      <div className="menu-list__icon-container">
+        <button
+          className={show ? "hamburger-active" : ""}
+          aria-expanded={show}
+          onClick={() => setShow(!show)}
+        >
+          <span class="sr-only">Open/Close menu</span>
+          <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+          </span>
+        </button>
+      </div>
+      <ul className={show ? "menu-list show-links" : "menu-list"}>
+        <li className="cv-li">
+          <a
+            aria-label="CV on Google Drive"
+            href="#"
+            rel="noopener"
+            target="_blank"
+          >
+            <FaDownload className="icon-style" />
+            <span> </span>CV
+          </a>
+        </li>
+        <li className="contact-li">
+          <a aria-label="Link to Email" rel="noopener" href="#"></a>
+        </li>
+      </ul>
+    </div>
+  )
 }
